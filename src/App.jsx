38528dd125332e600af9c1bfedc527a4fe288835
PhatVND <phat.vnduc@gmail.com>
2025-04-12 16:53:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
-import Devices from './pages/Devices';
-import Schedule from './pages/Schedule';
+import {  
+  RouterProvider 
+} from 'react-router-dom'
+import { router } from './routes/routers'
 
-const App = () => {
-  const [deviceData, setDeviceData] = useState({
-    currentDate: '2025-04-12',
-    currentTime: '14:32',
-    moisture: 59.6,
-    temperature: 32.4,
-    soil: 0
-  });
+
+function App() {
 
   return (
-    <div>
-      <Devices setDeviceData={setDeviceData} />
-      <Schedule deviceData={deviceData} />
-    </div>
-  );
-};
+      <>
+        <RouterProvider router={router} />
+      </>
+    ) 
+}
 
-export default App;
+export default App
