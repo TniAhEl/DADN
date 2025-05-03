fc09e66cfc56cd5150070f76b2e935a60fa9e0b9
PhatVND <phat.vnduc@gmail.com>
2025-05-04 06:56:19 +0700

    changed getLATEST

--- Backend/v1/routes/soil-moisure.route.js
+++ Backend/v1/routes/soil-moisure.route.js
@@ -7,7 +7,7 @@ const controller = require("../controllers/soil-moisure.controller.js");
 
 router.get("/", controller.index) 
 
-router.get("/latest", controller.index) 
+router.get("/latest", controller.getLatest) 
 
 
 module.exports = router;
